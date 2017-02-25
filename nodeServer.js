ea7202d6f175e55ae5611842f43f0f89efdf80cb
Nathan <nharrisanalyst@gmail.com>
2017-02-25 13:48:30 -0800

    dynamic set the port

--- nodeServer.js
+++ nodeServer.js
@@ -239,7 +239,9 @@ var server = http.createServer(function (req, res) {
 		})}	
 
 
-}).listen();
+}).listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 
